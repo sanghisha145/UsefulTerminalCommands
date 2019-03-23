b8385b5e55795967f4598fcaeba7cf902d6a4a5b
Divya Sanghi <divyasanghi145@gmail.com>
2019-03-23 19:16:11 +0530

    added awk also

--- OrderBySize.sh
+++ OrderBySize.sh
@@ -1,3 +1,6 @@
 #Sort hadoop directory by file size
 
 $ hdfs dfs -ls -S -h   hdfs://<directory_location> | sort -k6,7
+
+# grep only size,file_nme with size <2KB, here to avoid MB,GB, I didnt use flag "-h" with ls
+$ hdfs dfs -ls -S   hdfs://<directory_location> | sort -k6,7 | awk '{ if($5 < 2000) print $5,$8}'
